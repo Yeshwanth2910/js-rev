2c15906a19f926d6c78a8becd85b0bca242d0ed0
Yeshwanth2910 <135497818+Yeshwanth2910@users.noreply.github.com>
2024-08-23 16:13:54 +0000

    confirm config done

---
+++ 02_basics/variables.js
@@ -0,0 +1,6 @@
+let emailAdress = "yesh@gmail.com";
+const mbo = 98765;
+var state = "Andhra";
+let country;
+console.table([emailAdress,mbo,state,country]);
+/*prefer not to use VAR because of issue in block scope and functional scope */
