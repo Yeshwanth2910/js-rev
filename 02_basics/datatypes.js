92007de9170a079198199d526dd47ce8e4a02b2c
Yeshwanth2910 <135497818+Yeshwanth2910@users.noreply.github.com>
2024-08-23 17:35:08 +0000

    datatypes

---
+++ 02_basics/datatypes.js
@@ -0,0 +1,15 @@
+// alert(3+3) you are using nodejs
+let name = "yeshwanth"
+let age = 20
+let isAdult = true
+//number
+// string
+//bigint
+//boolean
+//null
+//undefined
+//symbol =>unique
+
+//object
+
+
